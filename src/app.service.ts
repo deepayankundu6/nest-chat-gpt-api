f0d4fe1044d0c56ffcd477c217d1d44912eecae9
Deepayan Kundu <deepayankundu6@gmail.com>
2024-01-27 10:54:48 +0530

    inital commit

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  health(): string {
+    return 'The server is up and running';
   }
 }
